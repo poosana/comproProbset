b3ea16466ef3b618e1ae6cb57b62ac46dc189174
poosana <piixd1@gmail.com>
2021-01-15 20:11:48 +0700

    .

--- Prob7/7_1.cpp
+++ Prob7/7_1.cpp
@@ -1,2 +1,20 @@
 #include<iostream>
-using name
+using namespace std;
+
+int main(){
+    char score;
+    cout<<"Enter your score: ";
+    cin>>score;
+    switch (score){
+        case 'A':cout<<"Awesome";
+        break;
+        case 'B':cout<<"Great";
+        break;
+        case 'C':cout<<"Good";
+        break;
+        case 'F':cout<<"Noob";
+        break;
+        default:cout<<"Error: Invalid input";
+    }
+    return 0;
+}
